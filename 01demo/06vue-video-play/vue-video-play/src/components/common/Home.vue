a3ceade637e853ad96ed8b1464272da2dd37e60f
wb-xyl431344 <wb-xyl431344@alibaba-inc.com>
2018-11-28 14:21:06 +0800

    vue-video-play 视频播放hls格式示例代码

---
+++ 01demo/06vue-video-play/vue-video-play/src/components/common/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wrapper">
+          <el-container style="height: 100%;">
+            <el-container>
+                <el-header >
+                    <v-head></v-head>
+                </el-header>
+                <el-main style="height: 100%;">
+                    <div class="content">
+                        <!--<transition name="move" mode="out-in">-->
+                            <router-view></router-view>
+                        <!--</transition>-->
+                    </div>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import vHead from './Header.vue'
+
+export default {
+  components: {
+    vHead
+  }
+}
+</script>
+<style>
+    .el-main{
+        padding: 0px;
+    }
+    .el-header{
+        padding: 0px;
+    }
+</style>
